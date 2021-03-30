d41d3e09190526553d4fdc92526917df78796a02
erikvw <ew2789@gmail.com>
2021-03-30 09:50:35 -0500

    setup

--- setup.py
+++ setup.py
@@ -13,13 +13,13 @@ with open(os.path.join(os.path.dirname(__file__), "VERSION")) as f:
 os.chdir(normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name="respond-model",
+    name="respond-africa",
     version=VERSION,
     author="Erik van Widenfelt",
     author_email="ew2789@gmail.com",
     packages=find_packages(),
     include_package_data=True,
-    url="https://github.com/respond-africa/respond-model",
+    url="https://github.com/respond-africa/respond-africa",
     license="GPL license, see LICENSE",
     description="Loss to follow up classes for the respond-africa specific clinicedc/edc",
     long_description=README,
@@ -33,9 +33,10 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
 )
