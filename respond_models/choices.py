151415da438a61858056a7b724f8296f23e12174
erikvw <ew2789@gmail.com>
2021-04-23 11:15:51 -0500

    drafts for euroQOL, sf12; glucose form validator

--- respond_models/choices.py
+++ respond_models/choices.py
@@ -1,6 +1,32 @@
-from edc_constants.constants import FASTING, NEVER, NON_FASTING
+from edc_constants.constants import FASTING, NEVER, NON_FASTING, OTHER
 
 from .constants import MORE_THAN_HALF, NEARLY_EVERYDAY, NOT_AT_ALL, SEVERAL_DAYS
+
+CONTACT = (
+    ("tel", "Telephone conversation"),
+    ("home", "Home visIt"),
+    ("relative_at_clinic", "Relative visited the health facility"),
+    ("patient_record", "Patient record / document"),
+    (OTHER, "Other"),
+)
+
+DEATH_LOCATIONS = (
+    ("home", "At home"),
+    ("hospital_clinic", "Hospital/clinic"),
+    (OTHER, "Elsewhere, please specify"),
+)
+
+FASTING_CHOICES = ((FASTING, "Fasting"), (NON_FASTING, "Non-fasting"))
+
+
+INFORMANT = (
+    ("spouse", "Spouse"),
+    ("Parent", "Parent"),
+    ("child", "Child"),
+    ("healthcare_worker", "Healthcare Worker"),
+    (OTHER, "Other"),
+)
+
 
 MISSED_PILLS = (
     ("today", "today"),
@@ -18,5 +44,3 @@ PHQ_CHOICES = (
     (MORE_THAN_HALF, "More than half the days"),
     (NEARLY_EVERYDAY, "Nearly everyday"),
 )
-
-FASTING_CHOICES = ((FASTING, "Fasting"), (NON_FASTING, "Non-fasting"))
